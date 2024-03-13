a847e6a73cee079415b01424f7e24f39f18d5ddd
menosbel <belenfernandezn@gmail.com>
2024-03-13 08:45:41 -0300

    migration: create url info

---
+++ src/main/resources/db/V1__create_url_info.sql
@@ -0,0 +1,8 @@
+CREATE TABLE url_info(
+      id              SERIAL PRIMARY KEY,
+      key_url         VARCHAR(20) NOT NULL,
+      long_url        VARCHAR(500) NOT NULL,
+      short_url       VARCHAR(50) NULL
+);
+
+CREATE UNIQUE INDEX url_info_key_uq ON url_info(LOWER(key_url));
